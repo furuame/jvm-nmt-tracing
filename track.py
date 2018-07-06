1a0cf2494a9d82836eb799000293dde419f4b421
zhenwei <zhenwei.tw@gmail.com>
2018-07-07 00:37:12 +0800

    Pass the argument by commandline

--- track.py
+++ track.py
@@ -2,29 +2,36 @@ import os
 import sys
 import time
 
-# JVM pid
-PID = sys.argv[1]
-
-# Sampling Period (sec)
-interval = 5
-
-# Sampling Times
-times = 100
-
-# Temp Output File
-tmpfile = "/tmp/tmp-nmt-%d" % (int(time.time()))
-
 # Keywords for each class of memory
-keyTotal = "Total"
-keyJavaHeap = "Heap"
-keyThread = "Thread"
-keyGC = "GC"
-keyInternal = "Internal"
+Target_list = ["Total", "Heap", "Thread", "GC", "Internal"]
 
-# JCMD Command
-cmdJCMD = "jcmd " + PID + " VM.native_memory"
+# Tracking <target> <times> Process <pid> every <period> sec
+def tracking(pid, target, period, times, tmpfile):
+    cmdJCMD = "jcmd " + pid + " VM.native_memory"
 
-for i in range(times):
-    print("%d / %d" % (i, times))
-    os.system(cmdJCMD + " | grep " + keyTotal + " >> " + tmpfile)
-    time.sleep(interval)
+    for i in range(times):
+        # os.system(cmdJCMD + " | grep " + target + " >> " + tmpfile)
+        print(cmdJCMD + " | grep " + target + " >> " + tmpfile)
+        time.sleep(period)
+
+if __name__ == "__main__":
+    # JVM pid
+    # TODO: Handling invlaid PID
+    PID = sys.argv[1]
+
+    # Target
+    TARGET = sys.argv[2]
+    if TARGET not in Target_list:
+        print("Not Available Target")
+        quit()
+
+    # Sampling Period (sec)
+    PERIOD = int(sys.argv[3])
+
+    # Sampling Times
+    TIMES = int(sys.argv[4])
+
+    # Temp Output File
+    tmpfile = "/tmp/tmp-nmt-%d" % (int(time.time()))
+
+    tracking(PID, TARGET, PERIOD, TIMES, tmpfile)
